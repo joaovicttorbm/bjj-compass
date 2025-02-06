49d21539f4f7adf6ad77cf9191c9bd88e297cda1
joaovicttorbm <joaovicttorbispo@gmail.com>
2025-02-06 13:54:42 -0300

    feat: validation to create user

---
+++ common/validator/userValidator.js
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+
+export const userSchemaValidation = z.object({
+    username: z.string().min(5, 'Username must be at least 5 characters long').max(255),
+    email: z.string().email('Invalid email format').min(3, "must be at least 3 characters long").max(255),
+    password: z.string().min(8, 'Password must be at least 8 characters long').max(255)
+  });
+  
