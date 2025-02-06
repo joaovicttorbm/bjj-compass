266c706092afc6d15185a917b5ebe1989cdb1dd1
joaovicttorbm <joaovicttorbispo@gmail.com>
2025-02-06 14:40:00 -0300

    feat: add crypt password

--- controller/userController.js
+++ controller/userController.js
@@ -1,19 +1,21 @@
 import { userSchemaValidation } from "../common/validator/userValidator.js";
 import asyncHandler from "../middlewares/asyncHandler.js";
-import { HTTPSTATUS } from "../config/http.config.js"; 
+import { HTTPSTATUS } from "../config/http.config.js";
 import userService from "../service/userService.js";
 
 const register = asyncHandler(async (req, res) => {
+  
   const body = userSchemaValidation.parse({
     ...req.body,
   });
 
-  const { username } = await userService.registerUserService(body);
-  console.log(username);
+  const newUser = await userService.registerUserService(body);
+  
+  console.log('User created:', newUser);
 
   return res.status(HTTPSTATUS.CREATED).json({
     message: "User registered successfully",
-    data: user,
+    data: newUser, 
   });
 });
 
--- service/userService.js
+++ service/userService.js
@@ -3,24 +3,34 @@ import { userSchemaValidation } from "../common/validator/userValidator.js";
 import userModel from "../database/models/userModel.js";
 import { BadRequestException } from "../common/utils/catch-error.js";
 import ErrorCode from '../common/enums/error-code.enum.js';
+import { hashValue } from '../common/utils/bcrypt.js';
 
 const registerUserService = async (registerData) => {
-    const { username, email, password } = registerData;
-  
-    const existingUser = await userModel.findOne({ email });
-  
-    if (existingUser) {
-      throw new BadRequestException(
-        "User already exists with this email",
-        ErrorCode.AUTH_EMAIL_ALREADY_EXISTS
-      );
-    }
-  
-    const newUser = await userModel.create({
-      username,
-      email,
-      password,
-    });
-  };
 
-  export default { registerUserService}
+  const body = userSchemaValidation.parse(registerData);
+
+  const { username, email, password } = body;
+
+
+  const existingUser = await userModel.findOne({ email });
+
+  if (existingUser) {
+    throw new BadRequestException(
+      "User already exists with this email",
+      ErrorCode.AUTH_EMAIL_ALREADY_EXISTS
+    );
+  }
+
+  const hashedPassword = await hashValue(password) ;
+
+
+  const newUser = await userModel.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  return newUser; 
+};
+
+export default { registerUserService };
