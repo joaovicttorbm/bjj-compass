cab219701d896095009a1b03376313ddb5e55046
joaovicttorbm <joaovicttorbispo@gmail.com>
2025-02-07 20:23:48 -0300

    refactor: userById Util

--- service/trainingService.js
+++ service/trainingService.js
@@ -4,10 +4,11 @@ import { BadRequestException } from "../common/utils/catch-error.js";
 import { trainingSchemaValidation } from "../common/validator/trainingValidation.js";
 import trainingModel from "../database/models/trainingModel.js";
 import userModel from "../database/models/userModel.js";
+import { findExistingUserById } from "../common/utils/findExistingUserById.js";
 
 
 const createTrainingService = async (trainingData) => {
-    // Valida os dados de entrada
+
     const { 
       date = Date.now(), 
       techniques, 
@@ -17,7 +18,7 @@ const createTrainingService = async (trainingData) => {
       user_id 
     } = trainingSchemaValidation.parse(trainingData);
   
-    const existingUser = await findExistingUser(user_id);
+    const existingUser = await findExistingUserById(user_id);
   
     return await createTraining({
       date, 
@@ -27,19 +28,6 @@ const createTrainingService = async (trainingData) => {
       notes, 
       user_id
     });
-  };
-  
-  const findExistingUser = async (userId) => {
-    if (!mongoose.Types.ObjectId.isValid(userId)) {
-      throw new BadRequestException("Invalid user ID", ErrorCode.INVALID_USER_ID);
-    }
-  
-    const user = await userModel.findById(userId);
-    if (!user) {
-      throw new BadRequestException("User not found", ErrorCode.USER_NOT_FOUND);
-    }
-  
-    return user;
   };
   
   const createTraining = async (trainingData) => {
