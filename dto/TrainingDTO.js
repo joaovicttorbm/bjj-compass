63c24ff7660772603d39e30d6c5b79f479bf70b4
joaovicttorbm <joaovicttorbispo@gmail.com>
2025-02-14 14:06:07 -0300

    refactor

--- dto/TrainingDTO.js
+++ dto/TrainingDTO.js
@@ -1,17 +1,17 @@
 class TrainingDTO {
-    constructor(Training) {
-      this.id = _id;
-      this.date = new Date(date).toISOString();
-      this.techniques = techniques || [];
-      this.durationMinutes = durationMinutes;
-      this.intensityLevel = intensityLevel;
-      this.notes = notes;
+    constructor(training) {
+      this.trainingid = training._id;
+      this.date = new Date(training.date).toISOString();
+      this.techniques = training.techniques || [];
+      this.durationMinutes = training.durationMinutes;
+      this.intensityLevel = training.intensityLevel;
+      this.notes = training.notes;
     }
   
     static from(data) {
         return Array.isArray(data) 
-          ? data.map(item => new GoalDTO(item))
-          : [new GoalDTO(data)]; 
+          ? data.map(item => new TrainingDTO(item))
+          : [new TrainingDTO(data)]; 
       }
   }
   
